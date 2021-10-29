569a1524e9851d729d62b81af8dfa50489afe442
udomsak <specialclub13@gmail.com>
2021-10-29 08:16:30 +0700

    add layout and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { NavBarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
