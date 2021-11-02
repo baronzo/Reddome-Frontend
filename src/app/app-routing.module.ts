5f966ed6119ec8d53970913d419645d1bb9a6140
Nitikon Wichiansawang <nitikon.wc@gmail.com>
2021-11-02 15:26:01 +0700

    createpostpage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './components/layout/layout.component';
+import { PostComponent } from './components/post/post.compnent';
 
 const routes: Routes = [
-  { path: '', component:LayoutComponent }
+  { path: '', component:LayoutComponent },
+  { path: 'post', component:PostComponent }
 ];
 
 @NgModule({
