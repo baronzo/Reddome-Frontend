c4383354adfeea710004124721b7b546648eaac3
jrapbit <67047826+jrapbit@users.noreply.github.com>
2021-11-08 20:17:01 +0700

    updated group component

--- src/app/components/group/group.component.ts
+++ src/app/components/group/group.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-group',
   templateUrl: './group.component.html',
-  styleUrls: ['./group.component.scss']
+  styleUrls: ['./group.component.sass']
 })
 export class GroupComponent implements OnInit {
 
